929c067ccf8fb9ac979509ff7311e820cb6b33d6
Fran Lucena <fran.lucena@gmail.com>
2014-02-03 10:51:54 +0100

    No se generan las vistas, el código esta comentado

--- konecta2app/sql/ejerciciosall.sql
+++ konecta2app/sql/ejerciciosall.sql
@@ -1,3 +1,4 @@
+/*
 CREATE VIEW EJERCICIOS_DE_CLASE
 AS SELECT * FROM konecta2app_ejerciciosall
 WHERE idtipoejercicios_id = "Ejercicios de clase";
@@ -13,3 +14,4 @@ WHERE idtipoejercicios_id = "Controles";
 CREATE VIEW GLOBALES
 AS SELECT * FROM konecta2app_ejerciciosall
 WHERE idtipoejercicios_id = "Globales";
+*/
