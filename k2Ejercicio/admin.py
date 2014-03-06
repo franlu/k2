31a84420ba6df45cf93e8d9e62d967723f8efccb
Fran Lucena <fran.lucena@gmail.com>
2014-03-06 10:06:00 +0100

    [ADD] Tema, Materia y Curso al admin de django

--- k2Ejercicio/admin.py
+++ k2Ejercicio/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from k2Ejercicio.models import Ejercicio, Dificultad, TipoEjercicio
+from k2Ejercicio.models import Ejercicio, Dificultad, TipoEjercicio, Tema, Materia, Curso
 
-admin.site.register(Dificultad)
+admin.site.register(Curso)
+admin.site.register(Materia)
+admin.site.register(Tema)
 admin.site.register(TipoEjercicio)
+admin.site.register(Dificultad)
 admin.site.register(Ejercicio)
 """admin.site.register(Notificacion)
 admin.site.register(Corregir)
