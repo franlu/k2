94cd5bd12bb6a3a0112daad4e0bf66c999690275
Fran Lucena <fran.lucena@gmail.com>
2014-03-06 18:16:03 +0100

    issue #16

--- k2Ejercicio/models.py
+++ k2Ejercicio/models.py
@@ -66,15 +66,11 @@ class Ejercicio(models.Model):
     tema = models.ForeignKey(Tema)
     dificultad = models.ForeignKey(Dificultad)
     tipo = models.ForeignKey(TipoEjercicio)
-    centro = models.CharField(max_length=10)
+    centro = models.CharField(max_length=10, blank=True)
     titulo = models.CharField(max_length=50)
-    imagen = models.ImageField(null=False, upload_to='k2Ejercicio/img/', max_length=24576)
     descripcion = models.CharField(max_length=3000)
-    resultado = models.CharField(max_length=500, null=True, blank=True)
-    calculadora = models.CharField(max_length=1)
-    interfaz = models.CharField(max_length=50)
-    consejo = models.CharField(max_length=200)
-    estado = models.CharField(max_length=30, null=True, blank=True)
+    herramientas = models.CharField(max_length=200)
+
 
     class Meta:
         verbose_name_plural = "Ejercicios"
@@ -93,45 +89,7 @@ class Notificacion(models.Model):
         verbose_name_plural = "Notificaciones"
 
     def __unicode__(self):
-        return u"%s" % self.idusuario
-
-"""
-class Corregir(models.Model):
-    idcorregir = models.AutoField(primary_key=True)
-    idusuario = models.ForeignKey(User)
-    idejercicio = models.ForeignKey(Ejercicio)
-    materia = models.ForeignKey(Materia)
-    resultado = models.CharField(max_length=500)
-    urlimagen = models.CharField(max_length=500)
-    estado = models.CharField(max_length=500)
-    fecha = models.DateTimeField()
-    numerointentos = models.CharField(max_length=100)
-    numeroimagenes = models.CharField(max_length=100)
-    numeropreguntas = models.CharField(max_length=100)
-    fallos = models.CharField(max_length=100)
-    nota = models.CharField(max_length=100)
-
-    class Meta:
-        verbose_name_plural = "Corregir"
-
-    def __unicode__(self):
-        return u"%s" % self.idusuario
-
-
-class EjerciciosPendientes(models.Model):
-    idpendientes = models.AutoField(primary_key=True)
-    idprofesor = models.ForeignKey(Profesor)
-    idejercicio = models.ForeignKey(Ejercicio)
-    idalumno = models.ForeignKey(User)
-    idcorregir = models.ForeignKey(Corregir)
-    fecha = models.DateTimeField()
-
-    class Meta:
-        verbose_name_plural = "Ejercicios pendientes"
-
-    def __unicode__(self):
-        return u"%s" % self.idpendientes
-"""
+        return u"%s" % self.usuario
 
 class Incidencias(models.Model):
     usuario = models.ForeignKey(User)
@@ -141,7 +99,7 @@ class Incidencias(models.Model):
         verbose_name_plural = "Incidencias"
 
     def __unicode__(self):
-        return u"%s" % self.idusuario
+        return u"%s" % self.usuario
 
 
 class Observaciones(models.Model):
@@ -152,7 +110,7 @@ class Observaciones(models.Model):
         verbose_name_plural = "Observaciones"
 
     def __unicode__(self):
-        return u"%s" % self.idusuario
+        return u"%s" % self.usuario
 
 
 class EstadoEjercicios(models.Model):
@@ -165,59 +123,59 @@ class EstadoEjercicios(models.Model):
         return u"%s" % self.nombre
 
 
-class ExamenesManager(models.Manager):
+class ExamenManager(models.Manager):
     def get_queryset(self):
-        return super(ExamenesManager, self).get_queryset().filter(idtipoejercicios=1)
+        return super(ExamenManager, self).get_queryset().filter(tipoejercicio=1)
 
 
-class ControlesManager(models.Manager):
+class ControlManager(models.Manager):
     def get_queryset(self):
-        return super(ControlesManager, self).get_queryset().filter(idtipoejercicios=2)
+        return super(ControlManager, self).get_queryset().filter(tipoejercicio=2)
 
 
-class EjerciciosClaseManager(models.Manager):
+class EjercicioClaseManager(models.Manager):
     def get_queryset(self):
-        return super(EjerciciosClaseManager, self).get_queryset().filter(idtipoejercicios=3)
+        return super(EjercicioClaseManager, self).get_queryset().filter(tipoejercicio=3)
 
 
-class GlobalesManager(models.Manager):
+class GlobalManager(models.Manager):
     def get_queryset(self):
-        return super(GlobalesManager, self).get_queryset().filter(idtipoejercicios=4)
+        return super(GlobalManager, self).get_queryset().filter(tipoejercicio=4)
 
 
-class EjerciciosAll(models.Model):
-    idejerciciosall = models.AutoField(primary_key=True)
-    idejercicio = models.ForeignKey(Ejercicio)
-    idprofesor = models.ForeignKey(Profesor)
-    idalumno = models.ForeignKey(Alumno)
-    idclase = models.ForeignKey(Curso)
-    idtipoejercicios = models.ForeignKey(TipoEjercicio)
+class EjercicioEnviado(models.Model):
+    ejercicio = models.ForeignKey(Ejercicio)
+    profesor = models.ForeignKey(Profesor)
+    alumno = models.ForeignKey(Alumno)
+    curso = models.ForeignKey(Curso)
+    tipoejercicio = models.ForeignKey(TipoEjercicio)
     materia = models.ForeignKey(Materia)
-    idestadoejercicio = models.ForeignKey(EstadoEjercicios)
+    estadoejercicio = models.ForeignKey(EstadoEjercicios)
     fecha_envio = models.DateTimeField()
     fecha_recibido = models.DateTimeField(null=True, blank=True)
-    imagen_alumno = models.CharField(max_length=500, null=True, blank=True)
-    imagen_profesor = models.CharField(max_length=500, null=True, blank=True)
     nota = models.CharField(max_length=10, null=True, blank=True)
-    bien_mal = models.CharField(max_length=10, null=True, blank=True)
-    resultado = models.CharField(max_length=500, null=True, blank=True)
+    bien_mal = models.BooleanField(null=True, blank=True)
     tiempo_realizacion = models.IntegerField(null=True, blank=True, default=0)
     tiempo_maximo = models.IntegerField(null=True, blank=True, default=0)
     intentos_posibles = models.IntegerField(null=True, blank=True, default=0)
     estado = models.CharField(max_length=500, null=True, blank=True)
-    numerointentos = models.IntegerField(null=True, blank=True, default=0)
-    numeroimagenes = models.IntegerField(null=True, blank=True, default=0)
-    numeropreguntas = models.IntegerField(null=True, blank=True, default=0)
+    intentos_realizados = models.IntegerField(null=True, blank=True, default=0)
     nfallos = models.IntegerField(null=True, blank=True, default=0)
 
     objects = models.Manager()
-    examenes = ExamenesManager()  # 1
-    controles = ControlesManager()  # 2
-    ejerciciosclase = EjerciciosClaseManager()  # 3
-    globales = GlobalesManager()  # 4
+    examenes = ExamenManager()  # 1
+    controles = ControlManager()  # 2
+    ejerciciosclase = EjercicioClaseManager()  # 3
+    globales = GlobalManager()  # 4
 
     class Meta:
         verbose_name_plural = "Todos los ejercicios"
 
     def __unicode__(self):
-        return u"%s" % self.idejerciciosall
+        return u"%s" % self.id
+
+
+class Pregunta(models.Model):
+    enunciado = models.CharField(max_length=2000, null=True, blank=True)
+    respuesta = models.CharField(max_length=2000, null=True, blank=True)
+    consejo = models.CharField(max_length=2000, null=True, blank=True)
