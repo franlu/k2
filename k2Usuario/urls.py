55d2cbb7078cc20d85880c131d473d79a3c0c7f0
Fran Lucena <fran.lucena@gmail.com>
2014-03-13 14:09:30 +0100

    [ADD] CRUD para alumno

--- k2Usuario/urls.py
+++ k2Usuario/urls.py
@@ -1,14 +1,20 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from k2Usuario.views import AlumnoList, AlumnosClaseList, ClaseList, ClaseCreate, ClaseUpdate, ClaseDelete
+from k2Usuario.views import AlumnoCreate, AlumnoUpdate, AlumnoDelete, AlumnoList, ClaseAlumnosList, ClaseList, ClaseCreate, ClaseUpdate, ClaseDelete
 
 urlpatterns = patterns('',
+    #Clase
+    url (r'^clases/$', login_required(ClaseList.as_view()), name='claselist'),
     url(r'^clases/crear/$', login_required(ClaseCreate.as_view()), name='clasecreate'),
     url(r'^clases/editar/(?P<pk>\d+)$', login_required(ClaseUpdate.as_view()), name='claseupdate'),
     url(r'^clases/borrar/(?P<pk>\d+)$', login_required(ClaseDelete.as_view()), name='clasedelete'),
-    url (r'^clases/$', login_required(ClaseList.as_view()), name='claselist'),
-    (r'^clases/alumnos/nuevo/$', 'k2Usuario.views.setAlumno'),
-    url(r'^clases/alumnos/$', login_required(AlumnoList.as_view()), name='alumnolist'),
-    url(r'^clases/alumnos/(?P<clase_id>\d+)/$', login_required(AlumnosClaseList.as_view()), name='alumnosclaselist')
+    url(r'^clases/alumnos/(?P<clase_id>\d+)/$', login_required(ClaseAlumnosList.as_view()), name='clasealumnoslist'),
+
+    #Alumno
+    url(r'^alumnos/$', login_required(AlumnoList.as_view()), name='alumnolist'),
+    url(r'^alumnos/crear/$', login_required(AlumnoCreate.as_view()), name='alumnocreate'),
+    url(r'^alumnos/editar/(?P<pk>\d+)$', login_required(AlumnoUpdate.as_view()), name='alumnoupdate'),
+    url(r'^alumnos/borrar/(?P<pk>\d+)$', login_required(AlumnoDelete.as_view()), name='alumnodelete'),
+
  )
--- k2Usuario/views.py
+++ k2Usuario/views.py
@@ -6,6 +6,7 @@ import django.http as http
 
 from django.core.urlresolvers import reverse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.template.context import RequestContext
@@ -32,15 +33,19 @@ class ClaseCreate(FormView):
         return render(request, self.template_name, {'form': form})
 
 class ClaseUpdate(FormView):
+    model = Clase
     template_name = 'k2Usuario/clase_update.html'
     form_class = ClaseForm
 
+    def get_object(self, queryset=None):
+        return get_object_or_404(self.model, pk=self.kwargs['pk'])
+
     def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
+        form = self.form_class(initial=self.initial, instance=self.get_object())
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST or None, instance=Clase)
+        form = self.form_class(request.POST or None, instance=self.get_object())
         if form.is_valid():
             form.save()
             return http.HttpResponseRedirect('/pizarra/clases/')
@@ -60,14 +65,11 @@ class ClaseDelete(DeleteView):
         return http.HttpResponseRedirect('/pizarra/clases/')
 
 class ClaseList(ListView):
+
     model = Clase
     context_object_name = 'clases'
 
-class AlumnoList(ListView):
-    model = Alumno
-    context_object_name = 'alumnos'
-
-class AlumnosClaseList(ListView):
+class ClaseAlumnosList(ListView):
 
     context_object_name = 'alumnos'
     template_name = 'k2Usuario/alumnos_clase_list.html'
@@ -83,11 +85,17 @@ class AlumnosClaseList(ListView):
         context['clase'] = self.clase
         return context
 
-@login_required
-def setAlumno(request):
+class AlumnoCreate(FormView):
+    template_name = 'k2Usuario/alumno_create.html'
+    form_class = AlumnoForm
+    second_form_class = UserCreationForm
 
-    data = None
-    if request.method == 'POST':
+    def get(self, request, *args, **kwargs):
+        al = self.form_class(initial=self.initial, prefix='alumno')
+        uf = self.second_form_class(initial=self.initial, prefix='usuario')
+        return render(request, self.template_name, {'uf': uf, 'af': al})
+
+    def post(self, request, *args, **kwargs):
         data = request
         us = UserCreationForm(data.POST, prefix='usuario')
         al = AlumnoForm(data.POST, data.FILES, prefix='alumno')
@@ -97,14 +105,60 @@ def setAlumno(request):
             alumno = al.save(commit=False)
             alumno.idusuario = usuario
             alumno.save()
-            return http.HttpResponseRedirect(reverse('pizarra'))
+            return http.HttpResponseRedirect(reverse('alumnolist'))
 
-    else:
-        us = UserCreationForm(prefix='usuario')
-        al = AlumnoForm(prefix='alumno')
+        return render(request, self.template_name, {
+            'uf': us,
+            'af': al,
+        })
 
+class AlumnoUpdate(FormView):
+    template_name = 'k2Usuario/alumno_create.html'
+    model = Alumno
+    second_model = User
+    form_class = AlumnoForm
+    second_form_class = UserCreationForm
 
-    return render(request, 'k2Usuario/nuevoalumno.html', {
-        'uf': us,
-        'af': al,
-    })
+    def get_object(self, queryset=None):
+        return get_object_or_404(self.model, pk=self.kwargs['pk'])
+    def second_get_object(self, queryset=None):
+        return get_object_or_404(self.second_model, pk=self.kwargs['pk'])
+
+    def get(self, request, *args, **kwargs):
+        al = self.form_class(initial=self.initial, prefix='alumno', instance=self.get_object())
+        uf = self.second_form_class(initial=self.initial, prefix='usuario', instance=self.second_get_object())
+        return render(request, self.template_name, {'uf': uf, 'af': al})
+
+    def post(self, request, *args, **kwargs):
+        data = request
+        us = UserCreationForm(data.POST, prefix='usuario')
+        al = AlumnoForm(data.POST, data.FILES, prefix='alumno')
+
+        if us.is_valid() and al.is_valid():
+            usuario = us.save()
+            alumno = al.save(commit=False)
+            alumno.idusuario = usuario
+            alumno.save()
+            return http.HttpResponseRedirect(reverse('alumnolist'))
+
+        return render(request, self.template_name, {
+            'uf': us,
+            'af': al,
+        })
+
+class AlumnoDelete(DeleteView):
+    template_name = 'k2Usuario/alumno_delete.html'
+
+    def get(self, request, *args, **kwargs):
+        alumno = get_object_or_404(Alumno, pk=self.kwargs['pk'])
+        return render(request, self.template_name, {'alumno': alumno})
+
+    def post(self, request, *args, **kwargs):
+        alumno = get_object_or_404(Alumno, pk=self.kwargs['pk'])
+        alumno.delete()
+        return http.HttpResponseRedirect(reverse('alumnolist'))
+
+class AlumnoList(ListView):
+    model = Alumno
+    context_object_name = 'alumnos'
+
