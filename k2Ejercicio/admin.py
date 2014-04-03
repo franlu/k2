d72a66d60b0130a7921fa18d9fc91aee158ba703
Fran Lucena <fran.lucena@gmail.com>
2014-04-03 17:13:12 +0200

    issue #18

--- k2Ejercicio/admin.py
+++ k2Ejercicio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from k2Ejercicio.models import Ejercicio, Dificultad, TipoEjercicio, Tema, Materia, Curso
+from k2Ejercicio.models import Ejercicio, Dificultad, TipoEjercicio, Tema, Materia, Curso, Contenido
 
 admin.site.register(Curso)
 admin.site.register(Materia)
@@ -7,6 +7,7 @@ admin.site.register(Tema)
 admin.site.register(TipoEjercicio)
 admin.site.register(Dificultad)
 admin.site.register(Ejercicio)
+admin.site.register(Contenido)
 """admin.site.register(Notificacion)
 admin.site.register(Corregir)
 admin.site.register(EjerciciosPendientes)
