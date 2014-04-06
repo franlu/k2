62ab03f23b1cffcf25cc91b4521f407f848a0c45
Fran Lucena <fran.lucena@gmail.com>
2014-04-07 00:17:16 +0200

    [ADD] Filtro para la descarga de video

--- k2utils/media.py
+++ k2utils/media.py
@@ -12,12 +12,35 @@ def get_video(urlvideo,id):
     try:
         video = pafy.new(urlvideo)
         name = video.title.replace(" ", "_").replace("\t", "_")
+        name = str(name).decode(encoding='utf-8',errors='strict')
         lname = "%s_%s" % (id, name)
         lpath = MEDIA_VIDEO + lname
-        best = video.getbest()
-        lpath = lpath + '.' + best.extension
-        best.download(filepath=lpath)
+        streams = video.streams
+
+        sbest = None
+        for s in streams:
+            if s.extension == 'mp4' and s.get_filesize() < 10485760:
+                lpath = lpath + '.' + s.extension
+                sbest = s
+                break
+            else:
+                if s.extension == '3gp' and s.get_filesize() < 10485760:
+                    lpath = lpath + '.' + s.extension
+                    sbest = s
+                    break
+
+        if sbest is not None:
+            sbest.download(filepath=lpath)
+        else:
+            mp4s = [ s for s in streams if s.extension == 'mp4']
+            menor = mp4s[0]
+            for s in mp4s:
+                if s.get_filesize < menor:
+                    menor = s
+                    lpath = lpath + '.' + s.extension
+            menor.download(filepath=lpath)
     except Exception,e:
+        print "SIN VIDEO"
         print e
 
     return lpath
